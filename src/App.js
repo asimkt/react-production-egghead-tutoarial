42a3bd05a000a2d9a92b5fea01cbec007dc7a8cf
Asim KT <aasimkt@gmail.com>
2017-02-04 08:36:04 +0530

    Complete react-render-a-react-ui-based-on-initial-state and prev lesson

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+       { id: 1, name: 'Learn JSX', isComplete: true},
+       { id: 2, name: 'Build App', isComplete: false},
+       { id: 3, name: 'Deploy!!', isComplete: false}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +20,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Todo-App">
+          <input type="text" />
+          <form className="Todo-List">
+            <ul>
+              {this.state.todos.map( todo =>
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/>
+                  {todo.name}
+                </li>
+              )}
+            </ul>
+          </form>
+        </div>
       </div>
     );
   }
