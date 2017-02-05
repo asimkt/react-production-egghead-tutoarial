a018083243b2487442393b3ac3c21fb7daeb6bc5
Asim KT <aasimkt@gmail.com>
2017-02-05 12:22:03 +0530

    Update-data-in-a-list-without-mutations

--- src/components/lib/todoHelpers.js
+++ src/components/lib/todoHelpers.js
@@ -1,3 +1,16 @@
 export const addTodo = (list, todo) => [...list, todo]
 
 export const generateId = () => Math.floor(Math.random() * 10000);
+
+export const findById = (id, list) => list.find(item => item.id === id)
+
+export const toggleTodo = (todo) => ({ ...todo, isComplete: !todo.isComplete})
+
+export const updateTodos = (todos, updatedTodo) => {
+	const updatedTodoIndex = todos.findIndex( item => item.id === updatedTodo.id);
+	return [
+		...todos.slice(0, updatedTodoIndex),
+		updatedTodo,
+		...todos.slice(updatedTodoIndex+1)
+	]
+}
--- src/components/lib/todoHelpers.test.js
+++ src/components/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo} from './todoHelpers'
+import {addTodo, findById, toggleTodo, updateTodos} from './todoHelpers'
 
 test('addTodo should add the passed todo the list', () => {
 	const startTodos = [
@@ -36,4 +36,93 @@ test('addTodo should not mutate the existing todo array', () => {
 	const result = addTodo(startTodos, newTodo);
 
 	expect(result).not.toBe(startTodos);
+});
+
+test('findById should return expected item from array', () => {
+	// Arrange
+	const todos = [
+		{ id: 1, name: 'Learn JSX', isComplete: false},
+		{ id: 2, name: 'Build App', isComplete: false},
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	]
+	const expectedTodo = 
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+
+	// Act
+	const result = findById(3, todos);
+
+	// expect
+	expect(result).toEqual(expectedTodo);
+});
+
+test('toggleTodo should toggle the isComplete property of a todo', () => {
+	// Arrange
+	const todo = 
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	const toggledTodo = 
+		{ id: 3, name: 'Deploy!!', isComplete: true}
+
+	// Act
+	const result = toggleTodo(todo);
+
+	// expect
+	expect(result).toEqual(toggledTodo);
+});
+
+test('toggleTodo should not mutate original todo', () => {
+	// Arrange
+	const todo = 
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	const toggledTodo = 
+		{ id: 3, name: 'Deploy!!', isComplete: true}
+
+	// Act
+	const result = toggleTodo(todo);
+
+	// expect
+	expect(result).not.toBe(todo);
+});
+
+test('updateTodos should update an item by id', () => {
+	// Arrange
+	const todos = [
+		{ id: 1, name: 'Learn JSX', isComplete: false},
+		{ id: 2, name: 'Build App', isComplete: false},
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	]
+	const updatedTodo =
+		{ id: 2, name: 'Build App', isComplete: true}
+	const updatedTodos = [
+		{ id: 1, name: 'Learn JSX', isComplete: false},
+		{ id: 2, name: 'Build App', isComplete: true},
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	]
+
+	// Act
+	const result = updateTodos(todos, updatedTodo);
+
+	// expect
+	expect(result).toEqual(updatedTodos);
+});
+
+test('updateTodos should not mutate original array', () => {
+	// Arrange
+	const todos = [
+		{ id: 1, name: 'Learn JSX', isComplete: false},
+		{ id: 2, name: 'Build App', isComplete: false},
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	]
+	const updatedTodo =
+		{ id: 2, name: 'Build App', isComplete: true}
+	const updatedTodos = [
+		{ id: 1, name: 'Learn JSX', isComplete: false},
+		{ id: 2, name: 'Build App', isComplete: true},
+		{ id: 3, name: 'Deploy!!', isComplete: false}
+	]
+
+	// Act
+	const result = updateTodos(todos, updatedTodo);
+
+	// expect
+	expect(result).not.toBe(todos);
 });
