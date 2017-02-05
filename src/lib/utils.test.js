2bee9ba21bf14e34d7b556075a67f01a57b51faf
Asim KT <aasimkt@gmail.com>
2017-02-05 13:59:12 +0530

    Create-a-pipe-function-to-enable-function-composition

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -1,7 +1,9 @@
-import {partial} from './utils'
+import {partial, pipe} from './utils'
 
-const add = (a,b) => a + b;
-const multiplyAll = (a, b, c) => a * b * c;
+const add = (a,b) => a + b
+const multiplyAll = (a, b, c) => a * b * c
+const inc = (a) => a + 1
+const dbl = (a) => a * 2
 
 test('partial applies the first argument AOT', () => {
 	const addOne = partial(add, 1);
@@ -17,4 +19,26 @@ test('partial should apply any number of arguments', () => {
 	const result = multiplyWithTenAndTwo(3);
 
 	expect(result).toBe(60);
+})
+
+test('pipe passes the inc to dbl', () => {
+	const incThenDbl = pipe(inc, dbl);
+
+	const result = incThenDbl(2);
+
+	expect(result).toBe(6);
+})
+test('pipe passes the dbl to inc', () => {
+	const dblThenInc = pipe(dbl, inc);
+
+	const result = dblThenInc(2);
+
+	expect(result).toBe(5);
+})
+test('pipe works for multiple functions', () => {
+	const incThenDblThenAddAndInc = pipe(add, inc, dbl, inc);
+
+	const result = incThenDblThenAddAndInc(2, 1);
+
+	expect(result).toBe(9);
 })
